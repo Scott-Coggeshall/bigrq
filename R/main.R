2f12249af93ed8e82e0e7621c3f54319c85d12dd
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2019-10-18 18:28:54 -0700

    add split_data function for splitting data into M chunks based on rows. call split_data function in main to split up input data

--- R/main.R
+++ R/main.R
@@ -1,6 +1,21 @@
-main <- function(X, y, M, intercept, maxiter){
+split_data <- function(data_set, n_chunks){
   
-   
+  indices <- rep_len(1:n_chunks, floor(nrow(data_set)/n_chunks) + (nrow(data_set) %% n_chunks))
+  
+  lapply(1:n_chunks, function(x) data_set[indices == x, ])
+  
+  
+  
+  
+}
+
+main <- function(dat, M, intercept, maxiter){
+  
+  dat_list <- split_data(data_set = dat, n_chunks = M)
+  
+  dat_inverses <- lapply(dat_list, function(x) solve(crossprod(x)))
+  
+  dat_iter <- foreach()
   iter <- 1
   beta_i <- eta_i <- matrix(0, nrow = p, ncol = M)
   beta_avg <- eta_avg <- rowMeans(beta_i)
