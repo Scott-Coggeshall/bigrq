468379a45316059f950f27ace444db91ed90f230
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2019-10-22 16:39:27 -0700

    create update_beta function for performing global beta update based on penalty function
    modify main to pass necessary lists to foreach, perform updating inside of while loop

--- R/main.R
+++ R/main.R
@@ -9,26 +9,70 @@ split_data <- function(data_set, n_chunks){
   
 }
 
-main <- function(dat, M, intercept, maxiter){
+
+update_beta <- function(penalty, pen_deriv, lambda, beta_mat, eta_avg){
   
+  M <- nrow(beta_mat)
+  
+  beta_vec <- rowMeans(beta_mat)
+  
+  if(penalty == "lasso"){
+    
+   update1 <- beta_vec + eta_avg/gamma - lambda/(M*gamma) 
+   update2 <- -beta_vec - eta_avg/gamma - lambda/(M*gamma)
+   max(0, update) - min(0, update)
+    
+  } else if(penalty == "scad" | penalty == "mcp"){
+      
+      
+      
+    } else{
+      
+      stop("You've supplied an unsupported penalty function.")
+      
+    }
+  
+  
+  
+}
+
+main <- function(dat, M, intercept, maxiter, gamma, lambda, tau){
+  
+  # splitting dat into M chunks so that these can be iterated over
+  # in the foreach loop
   dat_list <- split_data(data_set = dat, n_chunks = M)
+  
+  outcome_list <- lapply(dat_list, function(x) x[,1])
+  
+  designmat_list <- lapply(dat_list, function(x) x[, -1])
   
   dat_inverses <- lapply(dat_list, function(x) solve(crossprod(x)))
   
-  dat_iter <- foreach()
+
   iter <- 1
   beta_i <- eta_i <- matrix(0, nrow = p, ncol = M)
   beta_avg <- eta_avg <- rowMeans(beta_i)
+  
+  u_list <- r_list <- lapply(1:M, function(x) rep(0, nrow(designmat_list[[x]])))
+  
   while(iter < maxiter){
-    
-   iter_run <- foreach(dat_i = dat_iter) %dopar%{
+  
+   beta_global_i <- update_beta(penalty, pen_deriv, lambda, beta_i, eta_avg)
+   
+          
+   iter_run <- foreach(dat_i = dat_iter, outcome_i = outcome_list, design_i = designmat_list, u_i = u_list, r_i = r_list ) %dopar%{
      
-     
-     .Call()
+     n_i <- nrow(design_i)
+     ParamUpdates(betar = beta_i, etar = eta_i, xr = design_i, yr = outcome_i, dat = dat_i, ur = u_i, rr = r_i, beta = beta_global_i, alpha = alpha, tau = tau, n = n, ni = n_i )
      
      
    } 
     
+   beta_i <- t(sapply(iter_run, function(x) x$betai))
+   
+   eta_i <- t(sapply(iter_run, function(x) x$etai))
+   
+   iter <- iter + 1
     
   }
   
