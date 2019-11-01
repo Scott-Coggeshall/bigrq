cdaf4dd8e2d418c017ae6788b101091ab8a0b164
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2019-10-31 18:17:52 -0700

    create check_convergence function for checking whether function 'main' has reached convergence and can stop; current version assumes no intercept

---
+++ R/check_convergence.R
@@ -0,0 +1,17 @@
+check_convergence <- function(dat, rho, beta_vec, beta_old_vec, r_vec, u_vec, abstol, reltol){
+  
+  n <- nrow(dat)
+  xbeta <- dat[, -1]%*%beta_vec
+  xbeta_old <- dat[, -1]%*%beta_old_vec
+  
+  rnorm <- sqrt(sum((dat[, 1] - xbeta - r_vec)))
+  
+  snorm <- sqrt(sum((rho*(xbeta - xbeta_old))^2))
+  
+  compare_vals <- c(sqrt(sum((xbeta)^2)), sqrt(sum(r_vec^2)))
+  
+  epspri <- sqrt(n)*abstol + reltol*max(compare_vals)
+  epsdual <- sqrt(n)*abstol + reltol*sqrt(sum(u_vec^2))
+  
+  rnorm < epspri && snorm < epsdual
+}
