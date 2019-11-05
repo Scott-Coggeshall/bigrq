7c82b69a9b1aff7a396feccff100f2dcb82e075e
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2019-11-04 18:14:19 -0800

    add scad and mcp updates

--- R/update_beta.R
+++ R/update_beta.R
@@ -9,16 +9,19 @@ update_beta <- function(penalty, pen_deriv, lambda, rho, beta_mat, eta_avg){
     
     shrink(beta_vec + eta_avg/rho, lambda/(M*rho))
     
-  } else if(penalty == "scad" | penalty == "mcp"){
+  } else if(penalty == "scad"){
     
+    scad_deriv <- lambda*((abs(beta_vec) <= lambda)*1 + (rho*lambda - beta_vec)/((rho - 1)*lambda)*(abs(beta_vec) <=  rho*lambda))
     
-    
-  } else{
-    
-    stop("You've supplied an unsupported penalty function.")
+    shrink(beta_vec + eta_avg/rho, scad_deriv/(M*rho))
+    } else if(penalty == "mcp"){
+      mcp_deriv <- (lambda - abs(beta_vec)/rho)*(abs(beta_vec) <= rho*lambda)/rho
+      
+      shrink(beta_vec + eta_avg/rho, mcp_deriv/(M*rho))
+  } else{ 
+     stop("You've supplied an unsupported penalty function.")
     
   }
-  
   
   
 }
