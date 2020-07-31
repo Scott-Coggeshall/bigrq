d9085957f2a25ab16c64de94384f0e370dfdaad9
Scott Coggeshall <sscogges@uw.edu>
2020-07-31 12:38:43 -0700

    create skeleton of model fitting wrapper function

---
+++ R/bigrq.R
@@ -0,0 +1,24 @@
+bigrq <- function(args){
+  
+  ### Error checking block
+  
+  
+  ### Intermediate variable block
+  
+  
+  ### Model fitting block - call r_main_parallel here
+  
+  
+  ### Output processing block
+  
+  
+  ### return NULL for now
+  
+  NULL
+  
+  
+  
+  
+  
+  
+}
