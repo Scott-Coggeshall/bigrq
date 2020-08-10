a575726e9ce8580b4709b73dcb53f65b6fd28671
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2020-08-10 13:48:06 -0700

    add penalized argument, start output processing block

--- R/bigrq.R
+++ R/bigrq.R
@@ -1,16 +1,15 @@
 bigrq <- function(x, y, M = 1, intercept = TRUE, max_iter = 500, min_iter = 10, 
                   n_workers = floor(parallel::detectCores()/2), 
-                  lambda = 1, tau=0.5, rho = 1, alpha = 1, penalty = "lasso", inverses_read_path = NULL, 
+                  lambda = 1, tau=0.5, rho = 1, alpha = 1, penalty = "lasso", penalized = rep(T, ncol(x)), inverses_read_path = NULL, 
                   inverses_write_path = NULL, abstol = 0.00001, reltol = 0.0001){
   
   ### Error checking block
   if(class(try(as.matrix(x), silent = TRUE))[1] == "try-error") {
     stop("x could not be converted to matrix.")
   }
-  if(class(try(as.matrix(y), silent = TRUE))[1] == "try-error") {
-    stop("y could not be converted to matrix.")
-  }
-  if(nrow(x) != nrow(y)) {stop("Number of rows in x and y do not match.")}
+  
+  y <- drop(y)
+  if(nrow(x) != length(y)) {stop("Number of rows in x and does not match number of observations in y.")}
   if(!is.numeric(M)) {stop("M is not an integer or numeric value.")}
   if(!is.logical(intercept)) {stop("intercept is not a logical value.")}
   if(!is.numeric(max_iter)) {stop("max_iter is not an integer or numeric value.")}
@@ -43,16 +42,23 @@ bigrq <- function(x, y, M = 1, intercept = TRUE, max_iter = 500, min_iter = 10,
   if(intercept == TRUE) {
     int_col <- matrix(data = 1, ncol = 1, nrow = nrow(x))
     x <- cbind(int_col, x)
+    penalized <- c(F, penalized)
   }
   dat <- cbind(y, x)
   
   ### Model fitting block - call r_main_parallel here
-  # currently won't work due to inverse file paths not existing?
-  # model <- r_main_parallel(dat = dat, M = M, intercept = intercept, max_iter = max_iter, min_iter = min_iter, 
-  #                          n_workers = n_workers, lambda = lambda, tau = tau, rho = rho, alpha = alpha, penalty = penalty, 
-  #                          inverses_read_path = inverses_read_path, inverses_write_path = inverses_write_path, 
-  #                          abstol = 10, reltol = 10)
+
+  model <- r_main_parallel(dat = dat, M = M, intercept = intercept, max_iter = max_iter, min_iter = min_iter,
+                           n_workers = n_workers, lambda = lambda, tau = tau, rho = rho, alpha = alpha, penalty = penalty,
+                           penalized = penalized, inverses_read_path = inverses_read_path, inverses_write_path = inverses_write_path,
+                           abstol = 10, reltol = 10)
   
+
   ### Output processing block
-  return(alpha)
+  
+  output <- list()
+  output$coef <- model$coef
+  output$iter <- model$iter
+  
+  return(output)
 }
