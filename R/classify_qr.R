ee000c71e43b6c37a3311c1ff94f43a8d307a097
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2020-08-10 18:03:58 -0700

    document classify_qr function

--- R/classify_qr.R
+++ R/classify_qr.R
@@ -1,3 +1,17 @@
+#' Binary Classifications from Quantile Regression
+#' 
+#' \code{classify_qr} creates binary classifications from a quantile
+#' regression fit by comparing predicted values to a prespecified threshold.
+#' 
+#' @param newx a design matrix containing the input values for predictions.
+#' @param coef a vector or matrix containing the estimated regression coefficient values. 
+#' If a matrix (e.g. if you want classifications for model fits produced by a 
+#' sequence of tuning parameter values), then the each column should correspond
+#' to a different fit.
+#' @param y_transform a vectorized function to be applied to the predictions.
+#' @param threshold the threshold to compare the predictions to.
+#' 
+#' @return a vector or matrix of classifications.
 classify_qr <- function(newx, coef, y_transform, threshold){
   
   
