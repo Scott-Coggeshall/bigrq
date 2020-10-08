0782326fd2e1364c610d7f7cd1d0eda9309517ff
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2020-10-08 13:40:01 -0700

    allow for large p and small p versions of hbic

--- R/hbic.R
+++ R/hbic.R
@@ -6,14 +6,19 @@
 #' @param y vector containing the values of the outcome used in fitting the model.
 #' 
 #' @return a vector of HBIC values
-hbic <- function(coefs, tau, x, y ){
+hbic <- function(coefs, tau, x, y, largep ){
   
   n <- length(y)
   p <- nrow(coefs)
   checkloss <- colSums(check_loss((y - x%*%coefs), tau))
   df <- colSums(coefs != 0)
   
+  if(largep){
   log(checkloss) + log(log(n))*log(p)/(2*n)*df
+  } else{
+    
+    log(checkloss) + log(n)/(2*n)*df
+  }
   
   
 }
